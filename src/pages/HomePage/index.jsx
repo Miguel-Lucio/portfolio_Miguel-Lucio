05dbd5a14ca3eb7a4b57d7d2b15ad9fee2a4f33f
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2024-02-24 22:52:50 -0300

    feat: implementig Profile

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,10 +1,14 @@
 import { Header } from "../../components/Header";
+import { Profile } from "../../components/Profile";
 import styles from "./style.module.scss";
 
 export const HomePage = () => {
   return (
     <>
       <Header />
+      <main>
+        <Profile />
+      </main>
     </>
   );
 };
