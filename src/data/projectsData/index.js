6af07bf52ca02b9bae6e7f480940c3d8ee1ad225
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2024-02-27 15:48:52 -0300

    feat: create projectsData

---
+++ src/data/projectsData/index.js
@@ -0,0 +1,39 @@
+export const projectsData = [
+  {
+    name: "MyContacts Front End",
+    description:
+      "Projeto Full Stack onde usuários podem gerenciar seus contatos.",
+    techs: ["JavaScript", "React"],
+    repo: "https://github.com/Kenzie-Academy-Brasil-Developers/MyContacts-FrontEnd_Miguel-Lucio",
+    deploy: null,
+  },
+  {
+    name: "MyContacts API",
+    description:
+      "Projeto Full Stack onde usuários podem gerenciar seus contatos.",
+    techs: ["TypeScript", "Express", "TypeORM"],
+    repo: "https://github.com/Kenzie-Academy-Brasil-Developers/MyContacts-API_Miguel-Lucio",
+    deploy: null,
+  },
+  {
+    name: "Kenzie Hub",
+    description: "Projeto Front End de rede social de desenvolvedores.",
+    techs: ["JavaScript", "React"],
+    repo: "https://github.com/Kenzie-Academy-Brasil-Developers/kenzie-hub__Miguel-Lucio",
+    deploy: "https://react-entrega-kenzie-hub-miguel-lucio.vercel.app/",
+  },
+  {
+    name: "Nu-Kenzie",
+    description: "Projeto Front End de gerenciamento de despesas.",
+    techs: ["JavaScript", "React"],
+    repo: "https://github.com/Kenzie-Academy-Brasil-Developers/nu-kenzie__Miguel-Lucio",
+    deploy: "https://react-entrega-s1-nu-kenzie-miguel-lucio.vercel.app/",
+  },
+  {
+    name: "Hamburgueria",
+    description: "Projeto Front End com foco em consumo de API.",
+    techs: ["JavaScript", "React"],
+    repo: "https://github.com/Kenzie-Academy-Brasil-Developers/hamburgueria_Miguel-Lucio",
+    deploy: "https://react-entrega-s3-hamburgueria-miguel-lucio.vercel.app/",
+  },
+];
