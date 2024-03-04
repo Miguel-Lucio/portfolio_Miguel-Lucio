79294bf5ea3e6bef3394c25ec4dcf800771c96ee
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2024-03-04 19:47:48 -0300

    feat: ProjectList

---
+++ src/components/ProjectList/index.jsx
@@ -0,0 +1,27 @@
+import styles from "./style.module.scss";
+import { projectsData } from "../../data/projectsData/index.js";
+import { ProjectCard } from "./ProjectCard/index.jsx";
+
+export const ProjectList = () => {
+  return (
+    <section id="projects">
+      <div className={styles.listBox}>
+        <div className="container horizon">
+          <div className={styles.titleBox}>
+            <h2 className="title one  center">Projetos</h2>
+            <p className="text  base center">
+              Originalidade e
+              <span className="text  yellow center"> dedicação</span> em cada
+              detalhe
+            </p>
+          </div>
+          <ul className={styles.listContent}>
+            {projectsData.map((project) => (
+              <ProjectCard key={project.id} project={project} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
